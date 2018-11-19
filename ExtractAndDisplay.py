c624d958acc51e0c5d837224e705bc30ebfb34f1
Alejandro Diaz <alejandrodiaz@DESKTOP-ODD4ECB.utep.edu>
2018-11-18 20:33:19 -0700

    currently, the program will run the extracting, converting, and displaying in parallel by initialising the extractin, converting, and displaying all on different threads

--- ExtractAndDisplay.py
+++ ExtractAndDisplay.py
@@ -2,6 +2,7 @@
 
 import threading
 import cv2
+import time
 import numpy as np
 import base64
 import queue
@@ -9,6 +10,9 @@ import queue
 # globals
 outputDir    = 'frames'
 clipFileName = 'clip.mp4'
+
+finishedExtracting = False
+finishedConverting = False
 
 def extractFrames(fileName, outputBuffer):
     # Initialize frame count 
@@ -34,84 +38,88 @@ def extractFrames(fileName, outputBuffer):
         success,image = vidcap.read()
         print('Reading frame {} {}'.format(count, success))
         count += 1
-
+        time.sleep(0.01)
     print("Frame extraction complete")
+    finishedExtracting = True
 
 
 
-def convertFrames(fileName, inputBuffer, outputBuffer):
+def convertFrames(inputBuffer, outputBuffer):
     # initialize frame count
     count = 0    
+    while True:
+        while not inputBuffer.empty():
+            print("Converting frame {}".format(count))
 
-    while not inputBuffer.empty():
-        print("Converting frame {}".format(count))
+                # load the next file
+            frameAsText = inputBuffer.get()
 
-            # load the next file
-        frameAsText = inputBuffer.get()
+            # decode the frame 
+            jpgRawImage = base64.b64decode(frameAsText)
 
-        # decode the frame 
-        jpgRawImage = base64.b64decode(frameAsText)
+            # convert the raw frame to a numpy array
+            jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
 
-        # convert the raw frame to a numpy array
-        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+            inputFrame = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
 
-        inputFrame = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
+            # convert the image to grayscale
+            grayscaleFrame = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
 
-        # convert the image to grayscale
-        grayscaleFrame = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
+            success, jpgImage = cv2.imencode('.jpg', grayscaleFrame)
 
-        success, jpgImage = cv2.imencode('.jpg', grayscaleFrame)
+            #encode the frame as base 64 to make debugging easier
+            jpgAsText = base64.b64encode(jpgImage)
 
-        #encode the frame as base 64 to make debugging easier
-        jpgAsText = base64.b64encode(jpgImage)
+            # add the frame to the buffer
+            outputBuffer.put(jpgAsText)
 
-        # add the frame to the buffer
-        outputBuffer.put(jpgAsText)
-        
-        # # generate output file name
-        # outFileName = "{}/grayscale_{:04d}.jpg".format(outputDir, count)
+            time.sleep(0.01)
+            
+            count += 1
+        if finishedExtracting:
+            finishedConverting = True
+            return
 
-        # # write output file
-        # cv2.imwrite(outFileName, grayscaleFrame)
-
-        count += 1
-
-        # # generate input file name for the next frame
-        # inFileName = "{}/frame_{:04d}.jpg".format(outputDir, count)
 
 
 
 def displayFrames(inputBuffer):
     # initialize frame count
     count = 0
+    while True:
+        # go through each frame in the buffer until the buffer is empty
+        while not inputBuffer.empty():
+            # get the next frame
+            frameAsText = inputBuffer.get()
 
-    # go through each frame in the buffer until the buffer is empty
-    while not inputBuffer.empty():
-        # get the next frame
-        frameAsText = inputBuffer.get()
+            # decode the frame 
+            jpgRawImage = base64.b64decode(frameAsText)
 
-        # decode the frame 
-        jpgRawImage = base64.b64decode(frameAsText)
+            # convert the raw frame to a numpy array
+            jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+            
+            # get a jpg encoded frame
+            img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
 
-        # convert the raw frame to a numpy array
-        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-        
-        # get a jpg encoded frame
-        img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
+            print("Displaying frame {}".format(count))        
 
-        print("Displaying frame {}".format(count))        
+            # display the image in a window called "video" and wait 42ms
+            # before displaying the next frame
+            cv2.imshow("Video", img)
+            if cv2.waitKey(24) and 0xFF == ord("q"):
+                break
+            time.sleep(0.001)
+            count += 1
 
-        # display the image in a window called "video" and wait 42ms
-        # before displaying the next frame
-        cv2.imshow("Video", img)
-        if cv2.waitKey(42) and 0xFF == ord("q"):
-            break
+        if finishedExtracting and finishedConverting:
+            print("Finished displaying all frames")
+            # cleanup the windows
+            cv2.destroyAllWindows()
+            return
 
-        count += 1
 
-    print("Finished displaying all frames")
-    # cleanup the windows
-    cv2.destroyAllWindows()
+
+    
 
 # filename of clip to load
 filename = 'clip.mp4'
@@ -121,10 +129,23 @@ extractionQueue = queue.Queue()
 
 grayscaleQueue = queue.Queue()
 
-# extract the frames
-extractFrames(filename,extractionQueue)
 
-convertFrames(filename, extractionQueue, grayscaleQueue)
+extract = threading.Thread(name='extract', target=extractFrames, args=(filename, extractionQueue))
+convert = threading.Thread(name='conver', target=convertFrames, args=(extractionQueue, grayscaleQueue))
+display = threading.Thread(name='display', target=displayFrames, args=(grayscaleQueue))
 
-# display the frames
+extract.start()
+convert.start()
 displayFrames(grayscaleQueue)
+#display.start()
+
+# extract the frames
+# extractFrames(filename,extractionQueue)
+
+# convertFrames(filename, extractionQueue, grayscaleQueue)
+
+# # display the frames
+# displayFrames(grayscaleQueue)
+
+
+
